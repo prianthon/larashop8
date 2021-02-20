24911fb77037acc66a39a911e118636c1fa1db60
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2021-02-20 20:20:08 +0700

    crud category selesai

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,3 +27,8 @@ Route::match(["GET", "POST"], "/register", function (){
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::resource("users", UserController::class);
+
+Route::get('/categories/trash', [CategoryController::class, 'trash'])->name('categories.trash');
+Route::get('/categories/{id}/restore', [CategoryController::class, 'restore'])->name('categories.restore');
+Route::delete('/categories/{category}/delete-permanent', [CategoryController::class, 'deletePermanent'])->name('categories.delete-permanent');
+Route::resource('categories', CategoryController::class);
