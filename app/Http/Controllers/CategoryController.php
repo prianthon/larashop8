63c9908a022331a86854428cf0ab952b1d92f5d3
Pri Anton Subardio <prianthon@MacBook-Air-Pri.local>
2021-02-21 12:40:06 +0700

    manage books selesai

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -167,4 +167,11 @@ class CategoryController extends Controller
             return redirect()->route('categories.index')->with('status','Category permanently deleted');
         }
     }
+
+    public function ajaxSearch(Request $request){
+        $keyword = $request->get('q');
+        $categories = \App\Models\Category::where("name", "LIKE", "%$keyword%")->get();
+
+        return $categories;
+    }
 }
